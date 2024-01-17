1c0aface89096de6625ceed59071bb9481528560
rokdd <rokdd@gmx.ch>
2024-01-17 14:38:20 +0100

    T

--- gondola/__init__.py
+++ gondola/__init__.py
@@ -1,3 +1,8 @@
+#from . import xmlang
+from . import basic
+
+def test_gondola():
+    print("hello here is gondola")
 
 __version__="0.1.3"
 '''This initial version'''
--- gondola/gondola.py
+++
@@ -1,5 +0,0 @@
-#from . import xmlang
-#from . import basic
-
-def test_gondola():
-    print("hello here is gondola")
