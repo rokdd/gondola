06a901225131d16a7e6c092e18877384911a6937
rokdd <rokdd@web.de>
2024-01-30 13:07:54 +0100

    Udate

--- doc/conf.py
+++ doc/conf.py
@@ -22,7 +22,30 @@ release = '0.1'
 extensions = ['sphinx.ext.autodoc',
     'sphinx.ext.viewcode',
     'sphinx.ext.todo',
-    'myst_parser']
+    'myst_parser',
+    
+    'sphinx.ext.doctest',
+    'sphinx.ext.intersphinx',
+
+    'sphinx.ext.coverage',
+    'sphinx.ext.mathjax',
+    'sphinx.ext.ifconfig',
+
+    'sphinx.ext.githubpages',
+    'sphinx.ext.napoleon',]
+
+# Napoleon settings
+napoleon_google_docstring = True
+napoleon_numpy_docstring = True
+napoleon_include_init_with_doc = True
+napoleon_include_private_with_doc = True
+napoleon_include_special_with_doc = True
+napoleon_use_admonition_for_examples = False
+napoleon_use_admonition_for_notes = False
+napoleon_use_admonition_for_references = False
+napoleon_use_ivar = False
+napoleon_use_param = True
+napoleon_use_rtype = True
 
 templates_path = ['_templates']
 exclude_patterns = ['_build', 'Thumbs.db', '.DS_Store']
