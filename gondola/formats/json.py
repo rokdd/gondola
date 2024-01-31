b355faabf9d14ddf7286db27614a7a6b2ff71f38
rokdd <rokdd@web.de>
2024-01-31 12:16:33 +0100

    Note

--- gondola/formats/json.py
+++ gondola/formats/json.py
@@ -1,11 +1,23 @@
 import json
 
-def read(path,logger):
+def read(path,default={}):
+    """
+    read Reads a json file from a path
+
+    _extended_summary_
+
+    Args:
+        path (_type_): posix or string path to the file
+
+    Note:
+        Default value need to be used when file not exists
+    Returns:
+        object: json
+    """    
     with open(path) as fd:
         config_task = json.load(fd)
-        #logger.debug(pprint(config_task))
         return config_task
 
-def write(path,data,logger):
+def write(path,data):
     with open(path, 'w') as file:
         json.dump(data, file, indent = 4)
