f90d9d68ac428e0fe7ab4f235bf6721e815436bd
rokdd <rokdd@gmx.ch>
2024-02-01 16:12:39 +0100

    Version

--- doc/conf.py
+++ doc/conf.py
@@ -10,10 +10,12 @@ import os
 import sys
 sys.path.insert(0,os.path.abspath("."))
 
+from importlib.metadata import version
+
 project = 'gondola'
 copyright = 'rokdd'
 author = 'rokdd'
-release = '0.1'
+release = version("InstalledPackageName")
 
 
 # -- General configuration ---------------------------------------------------
