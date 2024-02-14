ace6b5d20c29f303b22dd273674db128bad710ef
rokdd <rokdd@web.de>
2024-02-14 10:41:41 +0100

    Fix test

--- tests/test_logger.py
+++ tests/test_logger.py
@@ -5,6 +5,6 @@ from gondola.loggers import lj
 
 def test_lj_typs():
     assert lj("hello","world") == "hello world"
-    assert lj({"hello":1},None,"world") == "{'hello': 1} None world"
+    assert lj({"hello":1},None,"world") == "\n{'hello': 1} None world"
 
 
