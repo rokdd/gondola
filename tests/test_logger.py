f60d99f5225541499dd917cc57d18fd154cb5b2a
rokdd <rokdd@web.de>
2024-02-14 08:43:57 +0100

    Uodate

--- tests/test_logger.py
+++ tests/test_logger.py
@@ -1,7 +1,9 @@
-import pytest  
-from gondola.loggers import lj  
-  
-def test_lj_typs():  
-    assert lj("hello","world") == "hello world"  
-    assert lj({"hello":1},None,"world") == "{'hello': 1} None world"  
+import pytest
+
+from gondola.loggers import lj
+
+
+def test_lj_typs():
+    assert lj("hello","world") == "hello world"
+    assert lj({"hello":1},None,"world") == "{'hello': 1} None world"
 
