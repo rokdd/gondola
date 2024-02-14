fc62c7aa0e731f441ff7ea72b13701424c6ae7d0
rokdd <rokdd@gmx.ch>
2024-02-14 10:52:07 +0100

    Update setup version

--- setup.py
+++ setup.py
@@ -3,10 +3,25 @@ from setuptools import find_namespace_packages, setup
 with open("README.md", "r") as fh:
     long_description = fh.read()
 
+import codecs
+import os.path
+
+def read(rel_path):
+    here = os.path.abspath(os.path.dirname(__file__))
+    with codecs.open(os.path.join(here, rel_path), 'r') as fp:
+        return fp.read()
+
+def get_version(rel_path):
+    for line in read(rel_path).splitlines():
+        if line.startswith('__version__'):
+            delim = '"' if '"' in line else "'"
+            return line.split(delim)[1]
+    else:
+        raise RuntimeError("Unable to find version string.")
 
 setup(
     name="gondola",
-    version="0.1.15",
+    version=get_version("gondola/__init__.py"),
     url="https://github.com/rokdd/gondola",
     author="rokdd",
     author_email="rokdd@gmx.ch",
